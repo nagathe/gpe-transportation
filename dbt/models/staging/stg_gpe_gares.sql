991f3e6a18f8a6d87eeea564f028da66afe53cd1
Agathe <gat.the.blues@gmail.com>
2026-06-02 18:56:27 +0200

    feat: dbt transformations - staging & marts

--- dbt/models/staging/stg_gpe_gares.sql
+++ dbt/models/staging/stg_gpe_gares.sql
@@ -1,23 +1,17 @@
--- models/staging/stg_gpe_gares.sql
 with source as (
-    select * from raw.gpe_gares_shapefile
+    select * from raw.gpe_gares
 ),
 
 cleaned as (
     select
-        code_gare,
         nom_gare,
-        ligne_gpe                           as ligne,
+        ligne_gpe       as ligne,
         interconnexion,
-        latitude::double precision          as latitude,
-        longitude::double precision         as longitude,
-        geometry                            as geom,
-        -- Nettoyage date
-        nullif(trim(date_diffu), '')        as date_diffusion
+        geometry        as geom,
+        st_y(geometry)  as latitude,
+        st_x(geometry)  as longitude
     from source
     where nom_gare is not null
-      and latitude is not null
-      and longitude is not null
 )
 
 select * from cleaned
--- dbt/models/staging/stg_insee_communes.sql
+++ dbt/models/staging/stg_insee_communes.sql
@@ -1,32 +1,16 @@
--- models/staging/stg_insee_communes.sql
-
 with source as (
     select * from raw.insee_communes
-),
-
-noms as (
-    select * from raw.commune_names
 )
 
 select
-    source."CODGEO"         as code_commune,
-    noms.nom_commune,
-    source."P22_POP"        as population,
-    source."MED_SL23"       as revenu_median,
-    source."P22_CHOM1564"   as nb_chomeurs,
-    source."P22_ACT1564"    as nb_actifs,
-    source."C22_PMEN"       as nb_menages,
-    -- Taux de chômage calculé ici une fois pour toutes
-    case
-        when source."P22_ACT1564" > 0
-        then round(
-            (source."P22_CHOM1564" / source."P22_ACT1564" * 100)::numeric, 2
-        )
-        else null
-    end                     as taux_chomage,
-    source.longitude,
-    source.latitude
+    code_commune,
+    population,
+    revenu_median,
+    nb_chomeurs,
+    nb_actifs,
+    nb_menages,
+    taux_chomage,
+    longitude,
+    latitude
 from source
-left join noms
-    on source."CODGEO" = noms.code_commune
-where source."CODGEO" is not null
+where code_commune is not null
